a590379802a17ef7d6bc1aed74a14dcc52f5ff8e
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-23 04:37:48 -0700

    Login layout and dash layout created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./components/Login";
+import DashLayout from "./components/DashLayout";
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />}></Route>
+        <Route path="login" element={<Login />}></Route>
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
