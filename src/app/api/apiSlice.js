c8fc0cb7f8e79bf6a63b1b4eaa5f31b175c9725d
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-31 10:32:52 -0700

    Fixed url typo

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://technotes-api-pxpd.onrender.com",
+  baseUrl: "https://technotes-api-pxpd.onrender.com/",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
