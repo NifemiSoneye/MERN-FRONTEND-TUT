5569eec2e3864f0f23f1d8d1a913bb4c32e7c34e
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-31 08:06:44 -0700

    Use title hook created

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,9 @@ import Prefetch from "./features/auth/Prefetch";
 import PersistLogin from "./features/auth/PersistLogin";
 import RequireAuth from "./features/auth/RequireAuth";
 import { ROLES } from "./config/roles";
+import useTitle from "./hooks/useTitle";
 function App() {
+  useTitle("Dan D. repairs");
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
