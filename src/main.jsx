59cf53493f8b90e4ebb6794e751204c5d7334c8a
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-31 10:11:00 -0700

    Fixed env typo

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { store } from "./app/store.js";
 import { Provider } from "react-redux";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
-if (proccess.env.NODE_ENV === "production") disableReactDevTools();
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
